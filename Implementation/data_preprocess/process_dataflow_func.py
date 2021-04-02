63fa7c0943dbce45c4eb6e74572a807287143d91
SySeVR <36064662+SySeVR@users.noreply.github.com>
2021-04-02 15:03:09 +0800

    Update process_dataflow_func.py

--- Implementation/data_preprocess/process_dataflow_func.py
+++ Implementation/data_preprocess/process_dataflow_func.py
@@ -2,7 +2,6 @@
 '''
 This python file is used to precess the vulnerability slices, including read the pkl file and split codes into corpus.
 Run main function and you can get a corpus pkl file which map the same name slice file.
-================================================================
 '''
 
 import os
@@ -23,7 +22,6 @@ This function is used to split the slice file and split codes into words
 # Return
     [slices[], labels[], focus[]]
 '''
-
 def get_sentences(_path,labelpath,corpuspath,maptype=True):
     FLAGMODE = False
     if "SARD" in _path:
@@ -36,7 +34,7 @@ def get_sentences(_path,labelpath,corpuspath,maptype=True):
 
         filepath = os.path.join(_path, filename)
         f1 = open(filepath, 'r')
-        if FLAGMODE:
+        if FLAGMODE: 
             slicelists = f1.read().split("------------------------------")
         else:
             slicelists = f1.read().split("-------------------------")
@@ -46,23 +44,35 @@ def get_sentences(_path,labelpath,corpuspath,maptype=True):
             del slicelists[0]
         if slicelists[-1] == '' or slicelists[-1] == '\n' or slicelists[-1] == '\r\n':
             del slicelists[-1]
-			
+
+        filepath = os.path.join(labelpath, filename[:-4]+"_label.pkl")
+        f1 = open(filepath, 'rb')
+        labellists = pickle.load(f1)
+        f1.close()
+
+        if slicelists[0] == '':
+            del slicelists[0]
+        if slicelists[-1] == '' or slicelists[-1] == '\n' or slicelists[-1] == '\r\n':
+            del slicelists[-1]
+
         lastprogram_id = 0
         program_id = 0
         index = -1
-        file_name = 0
         slicefile_corpus = []
         slicefile_labels = []
         slicefile_focus = []
-        slicefile_func = []
         slicefile_filenames = []
+        slicefile_func = []
         focuspointer = None 
         for slicelist in slicelists:
             slice_corpus = []
             focus_index = 0
             flag_focus = 0 
+
             index = index + 1
+
             sentences = slicelist.split('\n')
+
             if sentences[0] == '\r' or sentences[0] == '':
                 del sentences[0]
             if sentences == []:
@@ -71,13 +81,12 @@ def get_sentences(_path,labelpath,corpuspath,maptype=True):
                 del sentences[-1]
             if sentences[-1] == '\r':
                 del sentences[-1]
-            label = int(sentences[-1])
-
             focuspointer = sentences[0].split(" ")[-2:]
             sliceid = index
             file_name = sentences[0]
+ 
             if FLAGMODE:    
-                program_id = sentences[0].split(" ")[1].split('/')[5]+sentences[0].split(" ")[1].split('/')[6]+sentences[0].split(" ")[1].split('/')[7]
+                program_id = sentences[0].split(" ")[1].split("/")[7]
             else:
                 program_id = sentences[0].split(" ")[1].split("/")[7]
             if lastprogram_id == 0:
@@ -86,16 +95,16 @@ def get_sentences(_path,labelpath,corpuspath,maptype=True):
             if not(lastprogram_id == program_id):
                 folder_path = os.path.join(corpuspath, str(lastprogram_id))
                 savefilename = folder_path + '/' + filename[:-4] + '.pkl'
-                if lastprogram_id not in os.listdir(corpuspath):
+                if lastprogram_id not in os.listdir(corpuspath):    
                     os.mkdir(folder_path)
-                if savefilename not in os.listdir(folder_path):
+                if savefilename not in os.listdir(folder_path):    
                     f1 = open(savefilename, 'wb')               
                     pickle.dump([slicefile_corpus,slicefile_labels,slicefile_focus,slicefile_func,slicefile_filenames], f1)
                 else:
-                    f1 = open(savefilename, 'rb')
-                    data = pickle.load(f1)
+                    f1 = open(savefilename, 'rb')        
+                    data = cPickle.load(f1)
                     f1.close()
-                    f1 = open(savefilename, 'wb')
+                    f1 = open(savefilename, 'wb')              
                     pickle.dump([slicefile_corpus+data[0],slicefile_labels+data[1],slicefile_focus+data[2],slicefile_func+data[3],slicefile_filenames+data[4]], f1)
                 f1.close()
                 slicefile_corpus = []
@@ -104,16 +113,11 @@ def get_sentences(_path,labelpath,corpuspath,maptype=True):
                 slicefile_filenames = []
                 slicefile_func = []
                 lastprogram_id = program_id
-
             sentences = sentences[1:]
-    
             for sentence in sentences:
-
                 if sentence.split(" ")[-1] == focuspointer[1] and flag_focus == 0:
-                    flag_focus = 1
-
+                    flag_focus = 1  
                 sentence = ' '.join(sentence.split(" ")[:-1])
-
                 start = str.find(sentence,r'printf("')
                 if start != -1:
                     start = str.find(sentence,r'");')
@@ -133,20 +137,20 @@ def get_sentences(_path,labelpath,corpuspath,maptype=True):
                 if flag_focus == 1:
                     if "expr" in filename:
                         focus_index = focus_index + int(len(list_tokens)/2)
-                        flag_focus = 2 
+                        flag_focus = 2  
                         slicefile_focus.append(focus_index)
-                    else:             
+                    else:               
                         if focuspointer[0] in list_tokens:
                             focus_index = focus_index + list_tokens.index(focuspointer[0])
-                            flag_focus = 2 
+                            flag_focus = 2  
                             slicefile_focus.append(focus_index)
                         else:  
                             if '*' in focuspointer[0]:
                                 if focuspointer[0] in list_tokens:
                                     focus_index = focus_index + list_tokens.index(focuspointer[0].replace('*',''))
-                                    flag_focus = 2 
+                                    flag_focus = 2  
                                     slicefile_focus.append(focus_index)
-                                else:                                   
+                                else:                                    
                                     flag_focus = 0
                             else:
                                 flag_focus = 0
@@ -158,10 +162,9 @@ def get_sentences(_path,labelpath,corpuspath,maptype=True):
                 else:
                     slice_corpus = slice_corpus + list_tokens
 
-            if flag_focus == 0:
+	    if flag_focus == 0:
                 continue
-
-            slicefile_labels.append(label)
+            slicefile_labels.append(labellists[int(sliceid)])
             slicefile_filenames.append(file_name)
 
             if maptype:
@@ -180,26 +183,26 @@ def get_sentences(_path,labelpath,corpuspath,maptype=True):
 
         folder_path = os.path.join(corpuspath, str(lastprogram_id))
         savefilename = folder_path + '/' + filename[:-4] + '.pkl'
-        if lastprogram_id not in os.listdir(corpuspath): 
+        if lastprogram_id not in os.listdir(corpuspath):   
             os.mkdir(folder_path)
-        if savefilename not in os.listdir(folder_path):
-            f1 = open(savefilename, 'wb')
+        if savefilename not in os.listdir(folder_path):    
+            f1 = open(savefilename, 'wb')                 
             pickle.dump([slicefile_corpus,slicefile_labels,slicefile_focus,slicefile_func,slicefile_filenames], f1)
         else:
-            f1 = open(savefilename, 'rb')
-            data = cPickle.load(f1)
+            f1 = open(savefilename, 'rb')              
+            data = pickle.load(f1)
             f1.close()
-            f1 = open(savefilename, 'wb') 
+            f1 = open(savefilename, 'wb')                 
             pickle.dump([slicefile_corpus+data[0],slicefile_labels+data[1],slicefile_focus+data[2],slicefile_func+data[3],slicefile_filenames+data[4]], f1)
         f1.close()
 
 if __name__ == '__main__':
+    
     SLICEPATH = './data/data_source/SARD/'
     LABELPATH = './data/label_source/SARD/'
-    CORPUSPATH = './data/corpus/SARD/'
-
+    CORPUSPATH = './data/corpus/'
     MAPTYPE = True
 
     sentenceDict = get_sentences(SLICEPATH, LABELPATH, CORPUSPATH, MAPTYPE)
-	
+
     print('success!')
