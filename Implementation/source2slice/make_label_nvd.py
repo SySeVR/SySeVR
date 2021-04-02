a71c09bda8ec1ebedd56846eeed7d5e1ea4759ff
SySeVR <36064662+SySeVR@users.noreply.github.com>
2021-04-02 15:07:38 +0800

    Add files via upload

---
+++ Implementation/source2slice/make_label_nvd.py
@@ -0,0 +1,85 @@
+# -*- coding:utf-8 -*-
+import pickle
+import re
+import os
+
+def make_label(data_path,label_path,_dict):	
+
+	for filename in os.listdir(data_path):
+		filepath = os.path.join(data_path,filename)
+		_labels = {}
+		f = open(filepath,'r')
+		slicelists = f.read().split('------------------------------')
+		f.close()
+
+		labelpath = os.path.join(label_path,filename[:-4]+'_label.pkl')	
+
+		if slicelists[0] == '':
+			del slicelists[0]
+		if slicelists[-1] == '' or slicelists[-1] == '\n' or slicelists[-1] == '\r\n':
+			del slicelists[-1]
+    
+		for slice in slicelists:
+			sentences = slice.split('\n')
+			if sentences[0] == '\r' or sentences[0] == '':
+				del sentences[0]
+			if sentences == []:
+				continue
+			if sentences[-1] == '':
+				del sentences[-1]
+			if sentences[-1] == '\r':
+				del sentences[-1]
+        
+			slicename = sentences[0]
+			label = 0
+			key = slicename.split('/')[6]+'/'+slicename.split('/')[7]+'/'+slicename.split('/')[8]+'/'+slicename.split('/')[-1].split(' ')[0] #源程序的key
+			if key not in _dict.keys():
+				_labels[slicename] = 0
+				continue
+			if len(_dict[key]) == 0:
+				_labels[slicename] = 0
+				continue
+			sentences = sentences[1:]
+			for sentence in sentences:
+				if (is_number(sentence.split(' ')[-1])) is False:
+					continue
+				linenum = int(sentence.split(' ')[-1])  #源程序中的行号
+				vullines = _dict[key]
+				if linenum in vullines:
+					label = 1
+					_labels[slicename] = 1
+					break 
+			if label == 0:
+				_labels[slicename] = 0	
+	
+		with open(labelpath,'wb') as f1:
+			pickle.dump(_labels,f1)
+		f1.close()
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+ 
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+ 
+    return False	
+	
+if __name__ == '__main__':
+
+	with open('./vul_context_linux_kernel.pkl','rb') as f:
+		_dict = pickle.load(f)
+	f.close()
+	#print(_dict)
+
+	code_path = './source/data_source/linux_kernel/'  #源程序
+	label_path = './C/label_source/linux_kernel/'   #标签文件
+	
+	make_label(code_path,label_path,_dict)	
---
+++ Implementation/source2slice/make_label_sard.py
@@ -0,0 +1,113 @@
+## coding:utf-8
+import os
+import pickle
+
+def make_label(path, _dict):
+    print(path)
+    f = open(path, 'r')
+    slicelists = f.read().split('------------------------------')[:-1]
+    f.close()
+    
+    labels = []
+    if slicelists[0] == '':
+        del slicelists[0]
+    if slicelists[-1] == '' or slicelists[-1] == '\n' or slicelists[-1] == '\r\n':
+        del slicelists[-1]
+    
+    for slicelist in slicelists:
+        sentences = slicelist.split('\n')
+            
+        if sentences[0] == '\r' or sentences[0] == '':
+            del sentences[0]
+        if sentences == []:
+            continue
+        if sentences[-1] == '':
+            del sentences[-1]
+        if sentences[-1] == '\r':
+            del sentences[-1]
+            
+        slicename = sentences[0].split(' ')[1].split('/')[-4] + sentences[0].split(' ')[1].split('/')[-3] + sentences[0].split(' ')[1].split('/')[-2] + '/' + sentences[0].split(' ')[1].split('/')[-1]
+        sentences = sentences[1:]
+        
+        label = 0
+        
+        if slicename not in _dict.keys():
+            continue
+        else:
+            vulline_nums = _dict[slicename]
+            for sentence in sentences:
+                if (is_number(sentence.split(' ')[-1])) is False:
+					continue
+                linenum = int(sentence.split(' ')[-1])
+                if linenum not in vulline_nums:
+                    continue
+                else:
+                    label = 1
+                    break
+        labels.append(label)
+    
+    return labels#返回标签
+       
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+ 
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+ 
+    return False
+       
+def main():
+    f = open("./contain_all.txt", 'r')
+    vullines = f.read().split('\n')
+    f.close()
+
+    _dict = {}
+    for vulline in vullines:
+        key = vulline.split(' ')[0].split('/')[-4] + vulline.split(' ')[0].split('/')[-3] + vulline.split(' ')[0].split('/')[-2] + '/' + vulline.split(' ')[0].split('/')[-1]
+        linenum = int(vulline.split(' ')[-1])
+        if key not in _dict.keys():
+            _dict[key] = [linenum]
+        else:
+            _dict[key].append(linenum)
+
+    lang = './C/test_data/data_source_add/sard/'
+    
+    path = os.path.join(lang, 'api_slices.txt')
+    list_all_apilabel = make_label(path, _dict)
+    dec_path = os.path.join(lang, 'api_slices_label.pkl')
+    f = open(dec_path, 'wb')
+    pickle.dump(list_all_apilabel, f, True)
+    f.close()
+    
+    path = os.path.join(lang, 'array_slices.txt')
+    list_all_arraylabel = make_label(path, _dict)
+    dec_path = os.path.join(lang, 'array_slices_label.pkl')
+    f = open(dec_path, 'wb')
+    pickle.dump(list_all_arraylabel, f, True)
+    f.close()
+    
+    path = os.path.join(lang, 'pointer_slices.txt')
+    list_all_pointerlabel = make_label(path, _dict)
+    dec_path = os.path.join(lang, 'pointer_slices_label.pkl')
+    f = open(dec_path, 'wb')
+    pickle.dump(list_all_pointerlabel, f, True)
+    f.close()
+ 
+    path = os.path.join(lang, 'expr_slices.txt')
+    list_all_exprlabel = make_label(path, _dict)
+    dec_path = os.path.join(lang, 'expr_slices_label.pkl')
+    f = open(dec_path, 'wb')
+    pickle.dump(list_all_exprlabel, f, True)
+    f.close()
+    
+
+if __name__ == '__main__':
+    main()
